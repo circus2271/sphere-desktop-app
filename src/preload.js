6acb4a70b4d9573a14cc59ed6d2791bb009d20dc
John Doe <johndoe@example.com>
2024-04-29 17:58:34 +0300

    first commit

---
+++ src/preload.js
@@ -0,0 +1,12 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    // setTitle: (title) => ipcRenderer.send('set-title', title),
+    requestTimeUpdate: () => ipcRenderer.send('request-time-update'),
+    onTimeUpdate: (callback) => ipcRenderer.on('time-update', (_event, value) => callback(value))
+})
+
